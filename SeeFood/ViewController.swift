b8a21235ca1ed5fd35e8c3710d6339ee98146e39
Adam Moore <adammoore@adams-macbook-pro.local>
2018-05-08 17:42:16 -0600

    Initial commit within Xcode for SeeFood.

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Adam Moore on 5/8/18.
+//  Copyright © 2018 Adam Moore. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+
+// In order for 'UIImagePickerControllerDelegate' to work, it also has to have 'UINavigationControllerDelegate' with it.
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+}
+
