d37460422e910bdb4512c3df934bd478b96f6988
Adam Moore <adammoore@Adams-MacBook-Pro.local>
2018-06-25 15:40:39 -0600

    Latest commit for updating GitHub

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -46,10 +46,75 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             
             image.image = userPickedImage
             
+            // Convert the 'UIImage' into a 'CIImage', which is a "Core Image Image", which will allow us to use the CoreML and Vision framework to test the image.
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                
+                fatalError("Could not convert to CIImage")
+                
+            }
+            
+            // Detect the CIImage with the information and instructions listed below in the 'detect()' function we created.
+            detect(image: ciimage)
+            
         }
         
         // This dismisses the Image Picker VC, to go back to our main navigation VC.
         imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        // Create a new model object, using the 'VNCoreMLModel' (from Vision) container for 'Inceptionv3', using its 'model' property loaded up.
+        // This new model constant is what we are going to be using to classify our image.
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            
+            fatalError("Loading CoreMLModel failed.")
+            
+        }
+        
+        // Make a request to the 'VNCoreMLModel'
+        let request = VNCoreMLRequest(model: model) { (theRequest, theError) in
+            
+            // The original results are an array of [Any?], so we are casting it as an array of [VNClassificationObservation], which is a class that holds classification observations.
+            guard let results = theRequest.results as? [VNClassificationObservation] else {
+                
+                fatalError("Model failed to process image.")
+                
+            }
+            
+            
+            // The 'results' that it comes out with is an array of the different analyses that the computer comes up with to classify the image.
+            // The results are in order of confidence, so the first is the most confident description, the second is the next most confident description, etc.
+            // There is really not much need for the lesser confidence options for our purposes, so we just get the first result.
+            
+            // 'results.first' returns the [0] index of the array.
+            if let firstResult = results.first {
+                
+                // 'identifier' checks the actual description of the first element in the classification observations array, the string of description that it produces (not the other confidence percentage information, the numbers, and all the other jazz; just the string), and checks if it contains the string 'hotdog'
+                if firstResult.identifier.contains("hotdog") {
+                    
+                    self.navigationItem.title = "Hotdog!"
+                    
+                } else {
+                    
+                    self.navigationItem.title = "Not Hotdog!"
+                    
+                }
+                
+            }
+            
+        }
+        
+        // Passes the CIImage into 'Vision' to process. The handler then performs a series of requests, in the form of an array, on the image.
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        // Performs the request that we set above on the image that we pass into the 'VNImageRequestHandler()'
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
         
     }
     
